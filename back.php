5c6e960fcbdc20d7442262b6a5abd2497c114d05
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-12-23 15:37:04 +0800

    後臺標題管理整理

--- back.php
+++ back.php
@@ -74,7 +74,25 @@
 						</tr>
 					</tbody>
 				</table>
-				<?php include "./back/title.php "; ?>
+				<?php 
+				// 方法一
+				$do=$_GET['do']??'title';
+				// 方法二
+				// $do=(isset($_GET['do']))?$_GET['do']:'title';
+				// 方法三
+				// if(isset($_GET['do'])){
+				// 	$do=$_GET['do'];
+				// }else{
+				// 	$do='title';
+				// }
+				$file="./back/".$do.".php ";
+				if(file_exists(($file))){
+					include $file;
+				}else{
+					include "./back/title.php ";
+				}
+				
+				?>
 			</div>
 			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
--- back/ad.php
+++ back/ad.php
@@ -1,1 +1,25 @@
-ad
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">動態文字廣告管理</p>
+    <form method="post" target="back" action="?do=tii">
+        <table width="100%">
+            <tbody>
+                <tr class="yel">
+                    <td width="45%">網站標題</td>
+                    <td width="23%">替代文字</td>
+                    <td width="7%">顯示</td>
+                    <td width="7%">刪除</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增動態文字廣告"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </form>
+</div>
