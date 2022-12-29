a991e2548a0e4b9714fbeac91d91fa88d70bb029
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-12-29 10:37:12 +0800

    圖片上傳破圖問題解決

--- api/add_title.php
+++ api/add_title.php
@@ -2,7 +2,7 @@
 include "base.php";
 
 if(!empty($_FILES['img']['tmp_name'])){
-    move_uploaded_file($_FILES['img']['tmp_name'],"../uplaod/".$_FILES['img']['name']);
+    move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
     $img=$_FILES['img']['name'];
 }
 
