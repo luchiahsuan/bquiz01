38f7c1479115079a0652f9d5437a4e2c3a204ff0
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-12-30 15:39:10 +0800

    建立判斷消息是否超過五筆的機制,並提供more連到到更多消息頁

--- front/home.php
+++ front/home.php
@@ -43,8 +43,27 @@ foreach($mvs as$mv){
 	</div>
 	<div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
 		<span class="t botli">最新消息區
+			<?php
+			if($News->count(['sh'=>1]>5)){
+				echo "<a href='?do=news' style='float:right'>";
+				echo "More...";
+				echo "</a>";
+			}
+			
+			?>
 		</span>
 		<ul class="ssaa" style="list-style-type:decimal;">
+		<?php
+$news=$News->all(['sh'=>1]," limit 5 ");
+foreach($news as$n){
+	echo "<li>";
+	echo mb_substr($n['text'],0,25)."...";
+	echo "<span class='all' style='display:none'>";
+	echo $n['text'];
+	echo "</span>";
+	echo "</li>";
+}
+?>
 		</ul>
 		<div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 		<script>
