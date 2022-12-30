f07812f3230e01667e64cb673774268dadc18217
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-12-30 14:02:26 +0800

    在前台顯示動態文字廣告跑馬燈

--- front/home.php
+++ front/home.php
@@ -1,5 +1,13 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 				<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+				<?php
+				$ads=$Ad->all(['sh'=>1]);
+				foreach($ads as $ad){
+					echo $ad['text'];
+					echo "&nbsp;&nbsp;&nbsp;";
+				}
+				
+				;?>
 				</marquee>
 				<div style="height:32px; display:block;"></div>
 				<!--正中央-->
